9c362feba33a588a415315184b4d894967b53e2e
Øystein Skar Rosvold <skario.dev@gmail.com>
2026-04-14 13:28:30 +0200

    Intitial commit

---
+++ dockerIntro.js
@@ -0,0 +1,8 @@
+// Basic counter function to use, simple as possible
+
+let count = 0;
+
+setInterval(() => {
+  count++;
+  console.log(count);
+}, 1000);
---
+++ information.js
@@ -0,0 +1,30 @@
+// Docker is used to create containers, which are used to simulate a VM
+// Typically tailored to do ONE specific task efficiently, allegedly
+
+// DOcker desktop EATS performance, Quit when not in use
+
+/*
+
+containers er som en app, men som har et eget "operativsystem kopi" til å gjøre appen 
+
+
+dockerfile er en config fil for docker. 
+
+node:alpine er bare bones oppsett. 
+
+copy ./ ./ betyr "././blabla", velger source og destinasjon
+
+EXPOSE is to expose a port, so its accessible outside the container, like from a browser
+
+"Images" eller bilde er ikke et visuelt bilde, men heller reglene og det som trengs for å kjøre en container.
+En container kjører et bilde 
+
+docker build ./ For å lage bilde som hanver i docker desktop app
+
+docker build -t image_name ./ 
+For å legge til et navn til imaget
+
+
+
+
+*/
